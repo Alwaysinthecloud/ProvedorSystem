87c4c9ea254f3dcdcef2796d941137a9b54525a5
Marcelo Siqueira <siqueira.has@gmail.com>
2018-06-12 08:53:06 -0300

    Ajuste Fino

--- src/provedorsystem/Funcionario.java
+++ src/provedorsystem/Funcionario.java
@@ -13,15 +13,15 @@ public class Funcionario extends Pessoa implements InterfaceFuncionario {
 
     public Funcionario() {
     }
-    Scanner pedecpf = new Scanner(System.in);
+//    Scanner pedecpf = new Scanner(System.in);
     Scanner entrada = new Scanner(System.in);
-    Scanner pedenome = new Scanner(System.in);
-    Scanner endereco = new Scanner(System.in);
-    Scanner email = new Scanner(System.in);
-    Scanner cpfcan = new Scanner(System.in);
-    Scanner pederg = new Scanner(System.in);
-    Scanner pedetelefone = new Scanner(System.in);
-    Scanner pedesalario = new Scanner(System.in);
+//    Scanner pedenome = new Scanner(System.in);
+//    Scanner endereco = new Scanner(System.in);
+//    Scanner email = new Scanner(System.in);
+//    Scanner cpfcan = new Scanner(System.in);
+//    Scanner pederg = new Scanner(System.in);
+//    Scanner pedetelefone = new Scanner(System.in);
+//    Scanner pedesalario = new Scanner(System.in);
     ArrayList<Funcionario> listaf = new ArrayList();
 
     @Override
@@ -38,19 +38,20 @@ public class Funcionario extends Pessoa implements InterfaceFuncionario {
         Funcionario funcionario = new Funcionario();
 
         System.out.println("Informe o nome: ");
-        funcionario.setNome(pedenome.nextLine());
+        funcionario.setNome(entrada.nextLine());
         System.out.println("Informe o endereço: ");
-        funcionario.setEndereco(endereco.nextLine());
+        funcionario.setEndereco(entrada.nextLine());
         System.out.println("Informe o Email: ");
-        funcionario.setEmail(email.nextLine());
+        funcionario.setEmail(entrada.nextLine());
         System.out.println("Informe o CPF: ");
-        funcionario.setCpf(cpfcan.nextLine());
+        funcionario.setCpf(entrada.nextLine());
         System.out.println("Informe o RG: ");
-        funcionario.setRg(pederg.nextLine());
+        funcionario.setRg(entrada.nextLine());
         System.out.println("Informe o número de telefone: ");
-        funcionario.setnumTelefone(pedetelefone.nextLine());
+        funcionario.setnumTelefone(entrada.nextLine());
         System.out.println("Informe o salário: ");
-        funcionario.setSalario(pedesalario.nextFloat());
+        funcionario.setSalario(entrada.nextFloat());
+        entrada.nextLine();
         listaf.add(funcionario);
 
     }
@@ -58,7 +59,7 @@ public class Funcionario extends Pessoa implements InterfaceFuncionario {
     @Override
     public void removerFuncionario() {
         System.out.println("Informe o CPF do funcionário: ");
-        cpfc = pedecpf.nextLine();
+        cpfc = entrada.nextLine();
         int resultado = 0;
         int voltas = 0;
 
@@ -78,16 +79,17 @@ public class Funcionario extends Pessoa implements InterfaceFuncionario {
     @Override
     public void aumentarSalario() {
         System.out.println("CPF de quem irá receber aumento: ");
-        cpfc = pedecpf.nextLine();
+        cpfc = entrada.nextLine();
         System.out.println("Informe em R$ o valor do aumento: ");
         aumento = entrada.nextFloat();
+        entrada.nextLine(); //Limpando o teclado 
         int resultado = 0;
         int voltas = 0;
 
         for (int i = 0; i < listaf.size(); i++) {
             voltas++;
             if (listaf.get(i).getCpf().contains(cpfc)) {
-                listaf.get(i).setSalario(salario + aumento);
+                listaf.get(i).setSalario(listaf.get(i).getSalario() + aumento);
                 System.out.println("Novo salário: " + listaf.get(i).getSalario());
                 resultado++;
             } else if (voltas == listaf.size() && resultado == 0) {
@@ -104,7 +106,7 @@ public class Funcionario extends Pessoa implements InterfaceFuncionario {
         int voltas = 0;
         int resultado =0;
         System.out.println("Informe o CPF para busca: ");
-        cpfc = pedecpf.nextLine();
+        cpfc = entrada.nextLine();
                 for (int i = 0; i < listaf.size(); i++) {
             voltas++;
             if (listaf.get(i).getCpf().contains(cpfc)) {
@@ -115,7 +117,7 @@ public class Funcionario extends Pessoa implements InterfaceFuncionario {
                 System.out.println("CPF: " + listaf.get(i).getCpf());
                 System.out.println("RG: " + listaf.get(i).getRg());
                 System.out.println("Telefone: " + listaf.get(i).getnumTelefone());
-                System.out.println("Salário: " + listaf.get(i).getSalario());
+                System.out.println("Salário: R$ " + listaf.get(i).getSalario());
                 resultado++;
             } else if (voltas == listaf.size() && resultado == 0) {
                 System.out.println("O CPF informado não existe");
